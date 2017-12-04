0a5f35be8bd3f166c38c65760a05b833387dbccb
Paul Kotok and Zack Evans <zack@>
2017-12-04 15:09:18 -0800

    Add unordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
   var items = [];
+  // creting empty array
   $("#form").submit(function(event) {
     var userInput = $("input#item1").val();
     items.push(userInput);
     event.preventDefault();
+    });
+    // this is adding items into the array
   $("#button").click(function(event) {
     items.sort();
     var itemsUpper = items.map(function(item) {
       return item.toUpperCase();
     });
-  $("#list").text(itemsUpper);
-  });
+  //$("#list").text(itemsUpper);
+    itemsUpper.forEach(function(itemsUppe) {
+      $("#list").append("<li>" + itemsUppe + "</li>");
+    });
+  // sorts items , makees new array, makes upper case
+  // creates the
   });
 });
